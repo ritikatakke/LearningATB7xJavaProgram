ef89a17971b5d38ccc18ee4130a62f48a7edc688
RitikaTakke <rtritika03@gmail.com>
2024-07-11 19:48:50 +0530

    add new code

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //This is a single comment
+        //Single comment-This code will not execute or run
+        //2+2
+
+        /*
+        Multiple Comment
+         */
+    }
+}
